841792398b828ce84e1566a9ba2a17564350c92e
saddamluhar365-dotcom <saddamluhar365@gmail.com>
2026-09-15 13:09:06 +0530

    feat: add core package

---
+++ barberian/core/__init__.py
@@ -0,0 +1,5 @@
+"""Core agent execution primitives."""
+
+from .execution import ExecutionEngine, ExecutionResult, Task, TaskState
+
+__all__ = ["ExecutionEngine", "ExecutionResult", "Task", "TaskState"]
